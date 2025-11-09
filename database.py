adaad86351ce3912044bfc130a1ad7d7672b8438
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2025-11-09 16:56:03 +0500

    Format DATABASE_URL assignment for readability

--- database.py
+++ database.py
@@ -4,8 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Use environment variable if set, otherwise default to local Postgres (user 'apple' on mac)
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Awais123@database-1.c3so26oscjij.ap-southeast-2.rds.amazonaws.com:5432/database-1
-")
+DATABASE_URL = os.getenv(
+  "DATABASE_URL", 
+  "postgresql://postgres:Awais123@database-1.c3so26oscjij.ap-southeast-2.rds.amazonaws.com:5432/database-1")
 
 # Create engine and session factory
 engine = create_engine(DATABASE_URL, future=True)
