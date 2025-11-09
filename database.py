78fc324df00949ce46c817b073ec2f9040b6cc37
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2025-11-09 16:41:14 +0500

    Update DATABASE_URL for remote Postgres connection

--- database.py
+++ database.py
@@ -4,7 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Use environment variable if set, otherwise default to local Postgres (user 'apple' on mac)
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://apple@localhost/todo_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Awais123@database-1.c3so26oscjij.ap-southeast-2.rds.amazonaws.com:5432/database-1
+")
 
 # Create engine and session factory
 engine = create_engine(DATABASE_URL, future=True)
