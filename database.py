6a4b4c99062564a5ad9d13c016323392a3560a52
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2025-11-09 17:46:02 +0500

    Update DATABASE_URL for production database

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 # Use environment variable if set, otherwise default to local Postgres (user 'apple' on mac)
 DATABASE_URL = os.getenv(
   "DATABASE_URL", 
-  "postgresql+psycopg2://todo_database:Awais123@database-1.c3so26oscjij.ap-southeast-2.rds.amazonaws.com/database-1")
+  "postgresql+psycopg2://postgres:Awais123@my-todo-app.c3so26oscjij.ap-southeast-2.rds.amazonaws.com:5432/my-todo-app")
 
 # Create engine and session factory
 engine = create_engine(DATABASE_URL, future=True)
